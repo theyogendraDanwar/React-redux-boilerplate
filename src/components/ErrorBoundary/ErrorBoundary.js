aec186f0b4ed62fac3ca9761044d754550764326
Yogendra Danwar <yogendra.danwar@gmail.com>
2019-11-10 12:19:08 +0530

    Added Error Boundaries on the boilerplate.

---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+  state = {
+    err: false,
+    errinfo: null,
+  }
+  componentDidCatch(err, errinfo) {
+    this.setState({
+      err: true,
+      errinfo
+    })
+  }
+  render() {
+    const { showErrorText, children } = this.props
+    const { err } = this.state
+    return (
+      err ? <div className="error-section">Opps! {showErrorText}</div> : {...children}
+    )
+  }
+}
+
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
-import { ConnectedRouter } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-import * as serviceWorker from './serviceWorker';
-
-import configureStore from './redux/store';
-import RouteWithSubRoute from './utils/RouteWithSubRoute';
+import { ConnectedRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
+import configureStore from './redux/store'
+import RouteWithSubRoute from './utils/RouteWithSubRoute'
 import NotFound from './components/NotFound/NotFound'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
+import * as serviceWorker from './serviceWorker'
 import { routes } from './routes'
 
 import './index.css';
@@ -16,17 +16,18 @@ import './index.css';
 export const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Provider store={configureStore(history)}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        {routes.map((route) => (
-          <RouteWithSubRoute key={route.path} {...route} />
-        ))
-        }
-        <Route component={NotFound} />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>,
+  <ErrorBoundary showErrorText="Error Happened in the main section.">
+    <Provider store={configureStore(history)}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          {routes.map((route) => (
+            <RouteWithSubRoute key={route.path} {...route} />
+          ))
+          }
+          <Route component={NotFound} />
+        </Switch>
+      </ConnectedRouter>
+    </Provider></ErrorBoundary>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
